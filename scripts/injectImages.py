86aa2cf936076c5d9383413293f9f4a193451501
Jnaneshwar Das <Jnaneshwar.Das@gmail.com>
2019-03-25 14:43:19 -0700

    adding scripts folder for django runscript feature, management tasks

--- scripts/injectImages.py
+++ scripts/injectImages.py
@@ -3,35 +3,32 @@ import sys
 import glob
 import PIL.Image as PILImage
 from webclient.models import Image as Im, ImageSourceType, CategoryType
-import numpy as np 
+import numpy as np
 import os
 
 
 def run():
-	path = sys.argv[3]
-	ctr = 1
-	for filename in os.listdir(path):
-		path_basename = os.path.basename(os.path.normpath(path)) + '/'
-		print(path_basename, filename)
-		image_name = filename
-		sourceType = ImageSourceType.objects.filter(description="human")[0]
-		img = PILImage.open(path + image_name)
-		width, height = img.size
-		print(width,height,type(image_name))
-		description = "wildlife"
-		request_categories = ['wildlife']
-		category_list = [CategoryType.objects.get_or_create(category_name=category)[0] for category in request_categories]
+        path = sys.argv[3]
+        ctr = 1
+        for filename in os.listdir(path):
+                path_basename = os.path.basename(os.path.normpath(path)) + '/'
+                print(path_basename, filename)
+                image_name = filename
+                sourceType = ImageSourceType.objects.filter(description="human")[0]
+                img = PILImage.open(path + image_name)
+                width, height = img.size
+                print(width,height,type(image_name))
+                description = "wildlife"
+                request_categories = ['wildlife', 'sheep', 'elk','coyote','deer']
+                category_list = [CategoryType.objects.get_or_create(category_name=category)[0] for category in request_categories]
 
-		dbImage = Im(name=image_name, path='/static/' + path_basename, description=description,source=sourceType, width=width, height=height)
+                dbImage = Im(name=image_name, path='/static/' + path_basename, description=description,source=sourceType, width=width, height=height)
 
 
-		print(type(dbImage))
-		Im.save(dbImage)
-		dbImage.categoryType.add(*category_list)
-		Im.save(dbImage)
+                print(type(dbImage))
+                Im.save(dbImage)
+                dbImage.categoryType.add(*category_list)
+                Im.save(dbImage)
 
-		ctr = ctr + 1
+                ctr = ctr + 1
 
-#
-# img = Image(name=request.POST['image_name'], path=path, description=request.POST.get('description', default=''),
-# 			source=sourceType, width=width, height=height)
