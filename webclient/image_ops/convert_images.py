6b0d9da9de2d3818df74d97196a5c338869d832d
Jnaneshwar Das <Jnaneshwar.Das@gmail.com>
2019-03-31 20:18:23 -0700

    npy mask array now has multiple objects and categories. fixing empy mask bug for some images

--- webclient/image_ops/convert_images.py
+++ webclient/image_ops/convert_images.py
@@ -189,43 +189,54 @@ def image_label_string_to_SVG_string(DBStr, height=None, width=None, keepImage=F
 
 
 def image_labels_to_countable_npy():
-    _user = User.objects.filter(username='god')[0]
+    _user = User.objects.filter(username='sarah')[0]
     _labeler = Labeler.objects.filter(user=_user)[0]
     labels = ImageLabel.objects.filter(labeler=_labeler)
     foldername = 'npy'
 
     for label in labels:
-        print(label)
-        filename = '%s' % (label.parentImage.name)
-        print(filename)
-        svg = label.combined_labelShapes
-        paths = re.findall(SVGRegex.rePath, svg)
-        image, height, width = SVGDimensions(svg)
-        masks_ndarray = np.zeros((len(paths), height, width))
-        print(masks_ndarray.shape, len(paths))
-        for idx,path in enumerate(paths):
-            print(path[0])
-            img=WandImage(blob=image_string_to_SVG_string_file(image_label_string_to_SVG_string(path, height, width)))
-            img.resize(width,height)
-            # masks_ndarray[idx, :, :] = numpy.array(img.convert('png'))
-            # print(label.parentImage)
-            img.background_color = WandColor('white')
-            img.alpha_channel = 'remove'
-            # Convert to black and white
-            img.negate()
-            img.threshold(0)
-            img.format = 'png'
-            if not os.path.exists(settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername):
-                os.makedirs(settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername)
-            outputFilename = (
-                        settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername + '/' + filename + '_' + str(idx) + IMAGE_FILE_EXTENSION)
-            img.save(filename=outputFilename)
-            im = imageio.imread(outputFilename)
-            print(idx, path)
-            masks_ndarray[idx, :, :] = np.array(im)
-        outputFilenameNpy = (
-                settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername + '/' + filename + '.npy')
-        print(outputFilenameNpy)
+        parent_image = label.parentImage
+        filename = '%s' % parent_image.name.replace('.JPG', '')
+        categorylabels = label.categorylabel_set.all()
+        height = parent_image.height
+        width = parent_image.width
+        total_paths = 254
+        masks_ndarray = np.zeros((total_paths, height, width))
+        ctr = 0
+        for cat_id, categorylabel in enumerate(categorylabels):
+            svg = categorylabel.labelShapes
+            paths = []
+            poly = []
+            paths = re.findall(SVGRegex.rePath, svg)
+            poly = re.findall(SVGRegex.rePolygon, svg)
+            shapes = paths + poly
+            if len(paths) + len(poly) > 0:
+                for idx,path in enumerate(shapes):
+                    print(ctr, cat_id, idx, path)
+                    img=WandImage(blob=image_string_to_SVG_string_file(image_label_string_to_SVG_string(path, height, width)))
+                    img.resize(width,height)
+                    img.background_color = WandColor('white')
+                    img.alpha_channel = 'remove'
+                    img.negate()
+                    img.threshold(0)
+                    img.format = 'png'
+                    if not os.path.exists(settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername):
+                        os.makedirs(settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername)
+                    outputFilename = (
+                            settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername + '/' + filename + '_' + str(idx) + IMAGE_FILE_EXTENSION)
+                    img.save(filename=outputFilename)
+                    im = imageio.imread(outputFilename)
+                    masks = np.array(im)
+                    cat_id = categorylabel.categoryType_id
+                    cat_mask = np.where(masks == 255,cat_id , masks)
+                    masks_ndarray[ctr, :, :] = cat_mask
+                    ctr = ctr + 1
+                outputFilenameNpy = (
+                    settings.STATIC_ROOT + settings.LABEL_FOLDER_NAME + foldername + '/' + filename + '.npy')
+            else:
+                print(ctr, cat_id, 0, 'EMPTY')
+        masks_ndarray.resize(ctr, height, width)
+        print(masks_ndarray.shape)
         np.save(outputFilenameNpy,masks_ndarray)
 
 
